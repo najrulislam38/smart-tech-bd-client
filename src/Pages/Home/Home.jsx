9361468332d7a1177f22f4bb67206bfe2d3f6711
Md Najrul Islam <mdnajrulislam.4738@gmail.com>
2023-10-18 18:01:08 +0600

    added footer and offer section on the home page.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,14 @@
+import Footer from "../../Components/Footer/Footer";
+import Offer from "./Offer/Offer";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <>
+      <main className="footer max-w-screen-2xl mx-auto px-5 md:px-10 lg:px-20">
+        <Offer></Offer>
+      </main>
+      <Footer></Footer>
+    </>
   );
 };
 
---
+++ src/Pages/Home/Offer/Offer.jsx
@@ -0,0 +1,25 @@
+const Offer = () => {
+  return (
+    <div className="w-full flex flex-col lg:flex-row gap-10  justify-between items-center bg-gradient-to-r from-cyan-500 to-[#2550de]">
+      <div className="w-full lg:w-1/2">
+        <img src="/assets/iphone-15-pro-max.png" className="w-full" />
+      </div>
+      <div className="w-full lg:w-1/2 py-10 px-10 lg:px-0">
+        <div className=" text-white">
+          <h3 className="text-4x lg:text-5xl font-fira-sans uppercase mb-2">
+            Iphone 15 Pro Max
+          </h3>
+          <h2 className="text-4xl lg:text-6xl font-fira-sans font-semibold uppercase">
+            Up to 20% OFF
+          </h2>
+          <p className="text-base lg:text-xl text-gray-400 font-medium pt-3 pb-5">
+            Get 5% Bonus Coupon with No Min. Purchase Now <br /> Purchase Now
+          </p>
+          <button className="btn btn-accent">Shop Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Offer;
