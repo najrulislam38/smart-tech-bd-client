472c285a85e0b989f945b4d0bfa579bbc0f6f528
Md Najrul Islam <mdnajrulislam.4738@gmail.com>
2023-10-19 21:41:14 +0600

    some modification to the partner section1.

--- src/Pages/Home/Partners/Partners.jsx
+++ src/Pages/Home/Partners/Partners.jsx
@@ -5,23 +5,23 @@ const Partners = () => {
         <h2 className="text-4xl font-fira-sans font-medium mb-3">
           Trusted Partners
         </h2>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 py-12 border-t">
-          <div>
+        <div className="grid  md:grid-cols-3 lg:grid-cols-6 py-12 border-t">
+          <div className="flex justify-center items-center p-6">
             <img src="/assets/partners/1.webp" alt="" />
           </div>
-          <div>
+          <div className="flex justify-center items-center p-6">
             <img src="/assets/partners/2.webp" alt="" />
           </div>
-          <div>
+          <div className="flex justify-center items-center p-6">
             <img src="/assets/partners/3.webp" alt="" />
           </div>
-          <div>
+          <div className="flex justify-center items-center p-6">
             <img src="/assets/partners/4.webp" alt="" />
           </div>
-          <div>
+          <div className="flex justify-center items-center p-6">
             <img src="/assets/partners/5.webp" alt="" />
           </div>
-          <div>
+          <div className="flex justify-center items-center p-6">
             <img src="/assets/partners/6.webp" alt="" />
           </div>
         </div>
